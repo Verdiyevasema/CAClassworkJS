21784ae9961b84831d31ec1a39589df863c6167a
Verdiyevasema <verdiyevasma3@gmail.com>
2023-10-30 08:53:55 -0700

    Js first task

---
+++ 01.Data-Types&Variables&Operators/main.js
@@ -0,0 +1,39 @@
+let firstName = "Sema";
+let lastName = "Verdiyeva";
+let fullName = firstName + " " + lastName;
+let country = "Samkir"
+let age = "19"
+let job = "developer and teacher"
+let info = fullName + ". I am a " + job + ". I am live in " + country + ". I am years old " + age + ".";
+console.log(info)
+
+let length = 16; 
+console.log(typeof length) //number
+let color = "Brown"
+console.log(typeof color) //string
+
+
+let road = 8600;
+let speed = 120;
+let time = road / speed;
+console.log("The car will arrive in China in " + time + " minutes.")
+
+let birth = 2004;
+let current = 2023;
+let agee = 2023 - 2004;
+console.log("I will be either " + agee + ".")
+
+let Sema = 100;
+let Nezrin = 24;
+let Gulsen = 69;
+let Ayan = 38;
+let Tufan =59
+let medium = Sema + Nezrin + Gulsen + Ayan + Tufan / 5;
+console.log(medium)
+
+
+let name = "John";
+let admin;
+admin = name;
+
+ console.log(admin);
