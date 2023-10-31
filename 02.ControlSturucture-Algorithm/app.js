f26d8ddc60ba33189d4de53ad96eb5f1e77add6b
Verdiyevasema <verdiyevasma3@gmail.com>
2023-10-31 08:52:49 -0700

    Js if,else,else if

---
+++ 02.ControlSturucture-Algorithm/app.js
@@ -0,0 +1,71 @@
+//Task 1.
+// let waither = 50;
+// if (waither >= 32) {
+//   console.log(`hava ${waither} derecedir Dress thinly`);
+// } else if (waither >= 15) {
+//   console.log(`${waither} Wear a jacket`);
+// } else {
+//   console.log(`${waither}Wear thick clothes`);
+// }
+
+//Task 2.
+// let letter = "c"
+// if (letter === "a" || letter === "e" || letter === "i" || letter === "o" || letter === "u") {
+//     console.log(`${letter} Herf saitdir.`);
+// }
+// else {
+//     console.log(`${letter} Herf samitdir`);
+// }
+
+//Task 3.
+// let c = 251564165;
+// let s = 11111;
+// let k = 125;
+// if (k > s && k > c) {
+//   console.log(`${k} is greater`);
+// } else if (c > s && c > k) {
+//   console.log(`${c} is greater`);
+// } else {
+//   console.log(`${s} is greater`);
+// }
+
+//Task 4.
+// let result = 95;
+// if (result > 50 && result < 60) {
+//   console.log(`${result} Exam grade: F`);
+// } else if (result > 60 && result < 70) {
+//   console.log(`${result} Exam grade: D`);
+// } else if (result > 70 && result < 80) {
+//   console.log(`${result} Exam grade: C`);
+// } else if (result > 80 && result < 90) {
+//   console.log(`${result} Exam grade: B`);
+// } else if (result > 90 && result < 100) {
+//   console.log(`${result} Exam grade: A`);
+// }
+
+//Task 5.
+// let firstName = "Sema";
+// console.log(firstName.length);
+// if (firstName.length < 5) {
+//   console.log(`${firstName} Qisa adiniz var.`);
+// } else if (firstName.length < 10) {
+//   console.log(`${firstName} Orta uzunluqlu adiniz var.`);
+// } else {
+//   console.log(`${firstName} Adiniz cox uzundur.`);
+// }
+
+
+
+
+//Task 6.
+// let a = 5;
+// let b = 6;
+// let c = 2;
+
+// if (a + b > a + c && a + b > b + c) {
+//   console.log(`${a} + ${b} = ${a + b} is biggest a b`);
+// } else if (a + c > b + c && a + c > a + b) {
+//   console.log(`${a} + ${c} = ${a + c} is biggest a c `);
+// } else {
+//   console.log(`${b} + ${c} = ${b + c} is biggest b c`);
+// }
