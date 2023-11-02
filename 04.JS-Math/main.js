8c268de7e818f061efa128b23cc367d0d195ce2c
Verdiyevasema <verdiyevasma3@gmail.com>
2023-11-02 08:42:20 -0700

    JS-Math

---
+++ 04.JS-Math/main.js
@@ -0,0 +1,11 @@
+let num = 12.23;
+
+console.log(Math.floor(num)); // 12
+
+console.log(Math.pow(num, 2)); // 149.5729
+
+console.log(Math.sign(num)); //1
+
+console.log(Math.max(25, 15, 5)); //25
+
+console.log(Math.min(25, 15, 5)); //5
