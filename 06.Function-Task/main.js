d18eef41322896a6d54c6b484201b555dfbb41fc
Verdiyevasema <verdiyevasma3@gmail.com>
2023-11-07 06:06:28 -0800

    Array_metod-Task.

---
+++ 06.Function-Task/main.js
@@ -0,0 +1,78 @@
+// TASK 1:
+// function printFullName (firstName = "Sema", lastName = "Verdiyeva") {
+//     let fullName = firstName + " " + lastName
+//     return fullName;
+// }
+// console.log(printFullName("Sema", "Verdiyeva"));
+
+// TASK 2:
+// let sum = (num1, num2) => num1 + num2;
+// let multiply = (num1, num2) => num1 * num2;
+// let subtruct = (num1, num2) => num1 - num2;
+// let divide = (num1, num2) => num1 / num2;
+// console.log(sum(5,7));
+// console.log(multiply(5,7));
+// console.log(subtruct(5,7));* / - h
+// console.log(divide(5,7));
+
+// TASK 3:
+// function calculator(num1, num2, operator) {
+//   let result;
+//   if (operator === "+") {
+//     result = num1 + num2;
+//   } else if (operator === "-") {
+//     result = num1 - num2;
+//   } else if (operator === "/") {
+//     result = num1 / num2;
+//   } else if (operator === "*") {
+//     result = num1 * num2;
+//   } else {
+//     result = "Bele bir operator yoxdur";
+//   }
+//   return result;
+// }
+
+// console.log(calculator(5, 7, "+"));
+// console.log(calculator(5, 7, "-"));
+// console.log(calculator(5, 7, "/"));
+// console.log(calculator(5, 7, "*"));
+// console.log(calculator(5, 7, "vgh"));
+
+// TASK 4:
+// const employees = [
+//   { name: "Jamil", salary: 50000, department: "IT" },
+//   { name: "Jale", salary: 60000, department: "HR" },
+//   { name: "Bayram", salary: 55000, department: "IT" },
+//   { name: "Sahil", salary: 75000, department: "HR" },
+//   { name: "Maryam", salary: 65000, department: "IT" },
+//   { name: "Elnara", salary: 80000, department: "HR" },
+//   { name: "Davud", salary: 70000, department: "IT" },
+// ];
+
+// function filterEmployees(array) {
+//   let newArr = [];
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i].salary >= 60000) {
+//       newArr.push(array[i]);
+//     }
+//   }
+//   return newArr;
+// }
+
+// console.log(filterEmployees(employees));
+
+// TASK 5:
+// function reverseString(string) {
+//   let str = "";
+//   for (let i = string.length - 1; i >= 0; i--) {
+//     str += string[i];
+//   }
+
+//   return str;
+// }
+
+// console.log(reverseString("Hello World"));
+
+// TASK 6:
+let str =
+  "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi aut suscipit dolores.";
---
+++ 07.Array-Metod/main.js
@@ -0,0 +1,39 @@
+// TASK 1:
+// Tək ədədləri filter methodu vasitəsilə yeni bir dəyişkənə mənimsət:
+// ====>   [1, 3, 5, 7]
+// const number = [1, 2, 3, 4, 5, 6, 7, 8];
+// let newnumber = number.filter((item)=>item%2===1);
+// console.log(newnumber);
+
+// TASK 2:
+// Verilmiş array-də objectlərin first_name və last_name dəyişkənin aşağıdaki kimi göstərin.
+// =====> ["Mike Sheridan", "Tim Lee", "John Carte"]
+// const users = [
+//   {
+//     first_name: "Mike",
+//     last_name: "Sheridan",
+//   },
+//   {
+//     first_name: "Tim",
+//     last_name: "Lee",
+//   },
+//   {
+//     first_name: "John",
+//     last_name: "Carte",
+//   },
+// ];
+
+// let fullName = users.map((item) => `${item.first_name} ${item.last_name}`);
+// console.log(fullName);
+
+// TASK 3:
+// find metodu vasitəsilə array-in içindəki, yaşı 30-dan yuxarı olan ilk şəxsi "firstPerson" adlı dəyişkənə
+// mənimsədib console-a çıxardın. ====> { name: 'Aysu', age: 32 }
+// const persons = [
+//   { name: "Akif", age: 25 },
+//   { name: "Aysu", age: 32 },
+//   { name: "Ali", age: 35 },
+// ];
+
+// let firstPerson = persons.find((item) => item.age > 30);
+// console.log(firstPerson);
