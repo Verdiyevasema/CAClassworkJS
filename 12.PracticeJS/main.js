7780f38022cefdb06664b92df17f36962f878ec9
Verdiyevasema <verdiyevasma3@gmail.com>
2023-11-17 07:55:36 -0800

    PracticeJsTask

---
+++ 12.PracticeJS/main.js
@@ -0,0 +1,29 @@
+// ------------------------------------------------------- TASK 1 -------------------------------------------------------
+
+// Write a JavaScript function that reverse a number.
+// Sample Data and output:
+
+// Example x = 32243;
+// Expected Output : 34223
+
+// let number = 32243;
+// let result = 0;
+// result = Number(String(number).split("").reverse("").join(""));
+// console.log(result);
+
+// ------------------------------------------------------- TASK 2 -------------------------------------------------------
+
+// Write a JavaScript function that returns a passed string with letters in
+// alphabetical order.
+// Example string : 'webmaster'
+// Expected Output : 'abeemrstw'
+
+// let str = "webmaster";
+// console.log(str.split("").sort().join());
+
+// ------------------------------------------------------- TASK 3 -------------------------------------------------------
+
+// Write a JavaScript function that accepts a string as a parameter and converts the
+// first letter of each word of the string in upper case.
+// Example string : 'the quick brown fox'
+// Expected Output : 'The Quick Brown Fox '
