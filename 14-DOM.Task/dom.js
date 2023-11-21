c8af639eb4aa292945369d60b2e7889edd74fd34
Verdiyevasema <verdiyevasma3@gmail.com>
2023-11-21 07:30:00 -0800

    Dom task

---
+++ 14-DOM.Task/dom.js
@@ -0,0 +1,42 @@
+// TASK 1
+// let colorbox = document.querySelector(".box");
+// let btn = document.querySelector(".button");
+
+// colorbox.addEventListener("click", function () {
+//   let parentElement = colorbox.parentElement;
+//   parentElement.style.background = "green";
+//   parentElement.style.width = "300px";
+//   parentElement.style.height = "300px";
+//   parentElement.style.position = "relative";
+//   this.style.position = "absolute";
+//   this.style.bottom = "20px";
+//   this.style.right = "20px";
+// });
+
+// TASK 2
+// let btnTheme = document.querySelector(".btnTheme");
+
+// btnTheme.addEventListener("click", function () {
+//   document.body.classList.toggle("theme");
+// });
+
+// TASK 4
+
+// let city = document.querySelector(".city");
+// let ulElmt = document.createElement("ul");
+// ulElmt.style.listStyle = "none";
+
+// let liArr = ["Ganja", "Baku", "Yevlax", "Goranboy", "Shamkir"];
+
+// liArr.forEach((item) => {
+//   let liElmt = document.createElement("li");
+//   liElmt.innerText = item;
+
+//   liElmt.addEventListener("click", function () {
+//     this.remove();
+//   });
+
+//   ulElmt.append(liElmt);
+// });
+
+// city.append(ulElmt);
